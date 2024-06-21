74c0399873b4660e4eec9d35b80456f0ef4b3ea3
luis <lfscruz@outlook.com>
2024-06-21 19:47:18 -0300

    add diagramas

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import numpy as np
 from keras import models
 from matplotlib import pyplot as plt
 
-class CharacterRecognitionModel:
-    def __init__(self, model_path):
-        self.model = models.load_model(model_path)
+class KMNISTRun:
+    def __init__(self):
+        self.model_path = 'hiragana.model.keras'
+        self.class_labels = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ',
+                'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と',
+                'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ',
+                'ま', 'み', 'む', 'め', 'も', 'や', 'ゆ', 'よ', 'ら', 'り',
+                'る', 'れ', 'ろ', 'わ', 'を', 'ん', 'ゝ', 'ゞ', 'ー']
+        
+        self.model = models.load_model(self.model_path)
 
     def preprocess_image(self, image_path):
         # Load image using OpenCV
@@ -33,47 +40,33 @@ class CharacterRecognitionModel:
         # Process prediction...
         return prediction
 
-    def predict_character(self, prediction):
+    def interpret_prediction(self, prediction):
 
-        # Get the index of the predicted class
         predicted_class_index = np.argmax(prediction)
-        # Assuming you have a list of class labels in the same order as your model output
-        
-        class_labels = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ',
-                'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と',
-                'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ',
-                'ま', 'み', 'む', 'め', 'も', 'や', 'ゆ', 'よ', 'ら', 'り',
-                'る', 'れ', 'ろ', 'わ', 'を', 'ん', 'ゝ', 'ゞ', 'ー']
-        # Retrieve the predicted character label
-        predicted_character = class_labels[predicted_class_index]
+
+        predicted_character = self.class_labels[predicted_class_index]
         return predicted_character
     
-# Usage:
-model_path = 'hiragana.model.keras'
-image_paths = ['./images/1.jpg', './images/2.jpg', './images/3.jpg', './images/4.jpg', './images/5.jpg', './images/6.jpg', './images/7.jpg']
+    def show_prediction(self):       
+        image_paths = ['./images/1.jpg', './images/2.jpg', './images/3.jpg', './images/4.jpg', './images/5.jpg', './images/6.jpg', './images/7.jpg']
+
+        for image_path in image_paths:
+            # Load the image
+            image = cv2.imread(image_path)[:,:,0]
+            
+            # Display the image
+            image = np.invert(np.array([image]))
+            
+            plt.imshow(image[0], cmap=plt.cm.binary)
+            plt.show()
+            
+            # Make prediction
+            prediction = self.predict_image(image_path)
+            
+            # Process prediction
+            predicted_character = model.interpret_prediction(prediction)
+            print(f'O Hiragana digitado foi: {predicted_character}')
 
 # Create an instance of the model
-model = CharacterRecognitionModel(model_path)
-
-
-# Make predictions on new images
-for image_path in image_paths:
-    # Load the image
-    image = cv2.imread(image_path)[:,:,0]
-    
-    # Display the image
-    image = np.invert(np.array([image]))
-    
-    plt.imshow(image[0], cmap=plt.cm.binary)
-    plt.show()
-    
-    # cv2.imshow('Image', image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    
-    # Make prediction
-    prediction = model.predict_image(image_path)
-    
-    # Process prediction
-    predicted_character = model.predict_character(prediction)
-    print(f'O Hiragana digitado foi: {predicted_character}')
+model = KMNISTRun()
+model.show_prediction()
